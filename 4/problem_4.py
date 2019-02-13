819d943fdb8be2f7151cade707412848c03f70c8
Jihin Ip <jim_ip20@hotmail.com>
2019-02-13 18:59:06 +0000

    Completed problem 4

--- 4/problem_4.py
+++ 4/problem_4.py
@@ -8,3 +8,23 @@ class Problem_4(object):
         string = ''.join(list)
         return num == int(string)
 
+    def solver(self, num1, num2):
+        pal = []
+        n1 = 0
+        n2 = 0
+        while n1 <= num1:
+            answer = n1 * n2
+            if self.is_pal(answer) == True:
+                if answer not in pal:
+                    pal.append(answer)
+            while n2 <= num2:
+                answer = n1 * n2
+                if self.is_pal(answer) == True:
+                    if answer not in pal:
+                        pal.append(answer)
+                n2 += 1
+            n1 += 1
+            n2 = 1
+        print(max(pal))
+        return max(pal)
+
--- 4/test_problem_4.py
+++ 4/test_problem_4.py
@@ -7,7 +7,13 @@ class Test4(unittest.TestCase):
         n = problem_4.Problem_4()
         self.assertEqual(n.is_pal(9009), True)
 
+    def test_solver1(self):
+        n = problem_4.Problem_4()
+        self.assertEqual(n.solver(99, 99), 9009)
 
+    def test_solver2(self):
+        n = problem_4.Problem_4()
+        n.solver(999,999)
 
 if __name__ == '__main__':
     unittest.main()
