022bca1d381087f661755bc5b52c536769c0d6e4
Jihin Ip <jim_ip20@hotmail.com>
2019-02-13 14:04:34 +0000

    Fixing tests to give correct results: BELOW the number given

--- problem_1/problem_1.py
+++ problem_1/problem_1.py
@@ -1,3 +1,12 @@
 def problem_1(num):
-
-    pass
+    total = 0
+    count = num
+    while count > 0:
+        count -= 1
+        if count % 3 == 0:
+            total += count
+        elif count % 5 == 0:
+            total += count
+        print('printing count: {}'.format(count))
+        print('printing total: {}'.format(total))
+    return total
--- problem_1/test_problem_1.py
+++ problem_1/test_problem_1.py
@@ -1,11 +1,16 @@
 import unittest
-from problem_1.problem_1 import problem_1
+from problem_1 import problem_1
 
 class Test1(unittest.TestCase):
 
+    def test_3_returns_0(self):
+        self.assertEqual(problem_1(3), 0)
+
+    def test_5_returns_3(self):
+        self.assertEqual(problem_1(5), 3)
+
     def test_10_returns_23(self):
         self.assertEqual(problem_1(10), 23)
-
 
 if __name__ == '__main__':
     unittest.main()
