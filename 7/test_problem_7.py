0fb6752c3042db1f38c35f171c5a26deeb9d4d22
Jihin Ip <jim_ip20@hotmail.com>
2019-02-14 13:54:55 +0000

    Adding test for problem 7 for example

---
+++ 7/test_problem_7.py
@@ -0,0 +1,11 @@
+import unittest
+import problem_7
+
+class Test7(unittest.TestCase):
+
+    def test_aggregate_6_prime_numbers(self):
+        m = problem_7.Problem7()
+        self.assertEqual(m.aggregate_primes(6), 13)
+
+if __name__ == '__main__':
+    unittest.main()
