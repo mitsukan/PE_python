18e3b81f4f129d07528a3caf2c2cced32eda1b6c
Jihin Ip <jim_ip20@hotmail.com>
2019-02-16 12:29:59 +0000

    rewrite of problem 8 tests to break down and simplify

--- 8/test_problem_8.py
+++ 8/test_problem_8.py
@@ -3,8 +3,11 @@ import problem_8
 
 class Test8(unittest.TestCase):
 
-    def test_solve_example(self):
-        self.assertEqual(problem_8.solver(4), 5832)
+    def test_solve_first_4_index_in_blob(self):
+        self.assertEqual(problem_8.work_it_out(0,3), 126)
+
+    # def test_solve_example(self):
+    #     self.assertEqual(problem_8.solver(4), 5832)
 
 
 if __name__ == '__main__':
