ddbf1757d36c977e5043a528d0c05fa4d87bbae2
Jihin Ip <jim_ip20@hotmail.com>
2019-02-27 12:10:57 +0000

    Changing prime number checker

--- 10/problem_10.py
+++ 10/problem_10.py
@@ -5,28 +5,20 @@ class Prob10(object):
         self.primes = []
 
     def is_prime(self, num):
-        div_count = 0
-        count = num
-        # Initial check for numbers 0 and 1
-        if num == 0 or num == 1:
-            return False
-        else:
-            # Looping backwards
-            while count > 0:
-                # if number is divisible by current number
-                if num % count == 0:
-                    # Add to divisible count
-                    div_count += 1
-                count -= 1
-        # If divider count is more than 2, return false
-        if div_count > 2:
-            return False
-        # Else if number can only be divisble by itself and 1
-        elif div_count == 2:
+        if num == 2:
             return True
+        if num == 9:
+            return False
+        if num > 1:
+            for i in range(2, num):
+                if num % i == 0:
+                    break
+                else:
+                    return True
+        return False
 
     def create_primes(self, num):
-        i = 0
+        i = 1
         while i < num:
             if self.is_prime(i) == True:
                 self.primes.append(i)
