f6f0d28fcd0c350b3d913bf77a4dda28009e53e2
Jihin Ip <jim_ip20@hotmail.com>
2019-03-20 16:06:49 +0000

    Adding assertions and dictionary entries for single numbers

--- 17/problem_17.py
+++ 17/problem_17.py
@@ -1,4 +1,7 @@
 class Prob17(object):
 
+    def __init__(self):
+        self.dict = {1:3, 2:3, 3:5, 4:4, 5:4, 6:3, 7:5, 8:5, 9:4}
+
     def count(self, num):
-        return 3
+        return self.dict[num]
