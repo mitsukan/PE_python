da7b4b763519c2f210de57d402f6ba94be43278d
Jihin Ip <jim_ip20@hotmail.com>
2019-03-21 17:12:06 +0000

    Passing up to 13

--- 17/problem_17.py
+++ 17/problem_17.py
@@ -1,7 +1,8 @@
 class Prob17(object):
 
     def __init__(self):
-        self.dict = {1:3, 2:3, 3:5, 4:4, 5:4, 6:3, 7:5, 8:5, 9:4, 10:3}
+        self.dict = {1:3, 2:3, 3:5, 4:4, 5:4, 6:3, 7:5, 8:5, 9:4, 10:3, 11:6, 12:6, 13:8, 15:7, 18:8}
 
     def count(self, num):
-        return self.dict[num]
+        if num > 0 and num <= 13:
+            return self.dict[num]
--- 17/test_problem_17.py
+++ 17/test_problem_17.py
@@ -4,7 +4,7 @@ import problem_17
 
 class Test17(unittest.TestCase):
 
-    def test_can_count_letters_in_single_numbers(self):
+    def test_can_count_letters_in_unique_numbers(self):
         n = problem_17.Prob17()
         self.assertEqual(n.count(1), 3)
         self.assertEqual(n.count(2), 3)
@@ -16,3 +16,19 @@ class Test17(unittest.TestCase):
         self.assertEqual(n.count(8), 5)
         self.assertEqual(n.count(9), 4)
         self.assertEqual(n.count(10), 3)
+        self.assertEqual(n.count(11), 6)
+        self.assertEqual(n.count(12), 6)
+        self.assertEqual(n.count(13), 8)
+        # self.assertEqual(n.count(15), 7)
+        # self.assertEqual(n.count(18), 8)
+
+    # def test_can_count_letters_in_teen_numbers(self):
+    #     n = problem_17.Prob17()
+    #     self.assertEqual(n.count(14), 8)
+    #     self.assertEqual(n.count(16), 7)
+    #     self.assertEqual(n.count(17), 9)
+    #     self.assertEqual(n.count(19), 8)
+    #     self.assertEqual(n.count(20), 6)
+    #
+    # def test_can_count_letters_in_twenty_numbers(self):
+    #     n = problem_17.Prob17()
